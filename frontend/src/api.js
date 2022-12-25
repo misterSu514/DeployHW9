08675307ecfa15102288076a8cc429b44abe01e6
luca <luca2001514@gmail.com>
2022-12-26 01:07:46 +0800

    yes

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const API_ROOT =
   process.env.NODE_ENV === "production"
-    ? "deployhw9-production-7d2e.up.railway.app"
-    : "deployhw9-production-7d2e.up.railway.app";
+    ? "/"
+    : "http://localhost:4000/";
 
 const api = axios.create({ baseURL: API_ROOT });
 export default api;
