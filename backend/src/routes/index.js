6e172b6a3baea8a71434ce83c8cf730564129e30
luca <luca2001514@gmail.com>
2022-12-26 00:20:32 +0800

    testing

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import ScoreCardRouter from './scoreCard.js';
+import ScoreCardRouter from './ScoreCard.js';
 const router = Router();
 router.use('/', ScoreCardRouter);
 export default router
